0260bcc865776a820493edcafb1b291b8cd61073
gudanrenao <zhangwenit@126.com>
2019-04-26 09:27:13 +0800

    demo

--- src/main/java/com/zhangwenit/zhanglei/demo/api/ZhangleiDemoApiApplication.java
+++ src/main/java/com/zhangwenit/zhanglei/demo/api/ZhangleiDemoApiApplication.java
@@ -3,6 +3,9 @@ package com.zhangwenit.zhanglei.demo.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 张磊测试接口
+ */
 @SpringBootApplication
 public class ZhangleiDemoApiApplication {
 
